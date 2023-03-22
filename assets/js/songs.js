db0b974f0d296b56f2ab54ad13aba0f453cab4ac
IsaacAndra <isaac.andra84@gmail.com>
2023-03-22 19:17:22 -0300

    :sparkles: feat: Array de músicas

---
+++ assets/js/songs.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        src: './src/Warriors.mp3',
+        name: 'Warriors',
+    },
+    {
+        src: './src/Awaken.mp3',
+        name: 'Awaken',
+    },
+    {
+        src: "./src/Star-Walkin'.mp3",
+        name: "Star Walkin'",
+    },
+]
